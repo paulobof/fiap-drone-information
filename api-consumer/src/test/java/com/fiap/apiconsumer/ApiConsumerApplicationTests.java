09a38c15ab5dccb64b6d04be0073d46d02bce04b
Paulo Bof <paulobof@gmail.com>
2022-04-15 20:06:43 -0300

    Criação Estrutura

---
+++ api-consumer/src/test/java/com/fiap/apiconsumer/ApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.apiconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ api-productor/src/main/java/com/fiap/apiproductor/ApiProductorApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.apiproductor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProductorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiProductorApplication.class, args);
+    }
+
+}
